4a69216fb3b14dbaa4a1c3a2ef432f89ebc13109
Jason Daniels <jason.c.daniels@gmail.com>
2021-10-08 12:21:24 -0500

    Initial checkin after breaking out from Jcd.Utilities

---
+++ Jcd.Validations.Tests/TestHelpers/IntHolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Jcd.Validations.Tests.TestHelpers
+{
+   /// <summary>
+   ///    A helper class that implements IComparable
+   /// </summary>
+   public class IntHolder : IComparable<IntHolder>
+   {
+      #region Public Fields
+
+      public int Value;
+
+      #endregion Public Fields
+
+      #region Public Methods
+
+      public int CompareTo(IntHolder other) { return Value.CompareTo(other.Value); }
+
+      #endregion Public Methods
+
+      #region Public Constructors
+
+      public IntHolder() { }
+
+      public IntHolder(int v) { Value = v; }
+
+      #endregion Public Constructors
+   }
+}

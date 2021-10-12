d26d50ea9899fc3bcece2221c5afbbfe2f4798b4
Jason Daniels <jason.c.daniels@gmail.com>
2021-10-11 21:57:47 -0500

    Added an example project, examples to README.md, and README.md to the nuget package.

---
+++ Jcd.Validations.Examples/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+// ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+
+namespace Jcd.Validations.Examples
+{
+    public class Helper
+    {
+        private string _name;
+
+        public Helper(string name)
+        {
+            _name = Argument.IsNotNullWhitespaceOrEmpty(name, nameof(name));
+        }
+        
+        public void GetHelp()
+        {
+            Console.WriteLine($"{_name}: I'm helping!");
+        }
+    }
+    
+    public class Conductor
+    {
+        private Helper _helper;
+        private int _min;
+        private int _max;
+        private string _bossName;
+
+        public Conductor(Helper helper, int min, int max, string bossName)
+        {
+            _helper = Argument.IsNotNull(helper, nameof(helper));
+            _min = Argument.InRange(min, 1, 10,nameof(min));
+            _max = Argument.InRange(max, _min, 10,nameof(max));
+            _bossName = Argument.IsNotEmpty(bossName, nameof(bossName));
+        }
+
+        public void DoIt()
+        {
+            Console.WriteLine($"Conductor: I'm doing something useful between {_min} and {_max} times for {_bossName}, my boss.");
+            _helper.GetHelp();
+        }
+    }
+    
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // This will fail due to the null helper.
+            TryIt(()=>new Conductor(null,-1,-10,"").DoIt());
+            // This will fail due to Helper's name being empty.
+            TryIt(()=>new Conductor(new Helper(""),-1,-10,"").DoIt());
+            // This will fail due to min
+            TryIt(()=>new Conductor(new Helper("Helper"),-1,-10,"").DoIt());
+            // This will fail due to max being less than min
+            TryIt(()=>new Conductor(new Helper("Helper"),1,-10,"").DoIt());
+            // This will fail due to max also being less than min
+            TryIt(()=>new Conductor(new Helper("Helper"),5,1,"").DoIt());
+            // This will fail due to an empty boss name.
+            TryIt(()=>new Conductor(new Helper("Helper"),3,7,"").DoIt());
+            // This will succeed and output both messages.
+            TryIt(()=>new Conductor(new Helper("The Helper"),3,7,"Lucia").DoIt());
+        }
+
+        private static void TryIt(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # Jcd.Validations
-A set of C# helper classes to aid in parameter validation in methods.
+A helper class to aid in parameter validation in methods.
+
+## Example Constructor Argument Validations.
+```csharp
+    public class Conductor
+    {
+        ...
+        public Conductor(Helper helper, int min, int max, string bossName)
+        {
+            _helper = Argument.IsNotNull(helper, nameof(helper));
+            _min = Argument.InRange(min, 1, 10, nameof(min));
+            _max = Argument.InRange(max, _min, 10, nameof(max));
+            _bossName = Argument.IsNotEmpty(bossName, nameof(bossName));
+            ...
+```
 
 [![GitHub](https://img.shields.io/github/license/jason-c-daniels/Jcd.Validations)](https://github.com/jason-c-daniels/Jcd.Validations/blob/main/LICENSE)
 [![Build status](https://ci.appveyor.com/api/projects/status/djbghxj1ot3axbfs?svg=true)](https://ci.appveyor.com/project/jason-c-daniels/jcd-validations)
+[![CodeFactor Grade](https://img.shields.io/codefactor/grade/github/jason-c-daniels/Jcd.Validations)](https://www.codefactor.io/repository/github/jason-c-daniels/jcd.validations)
+
+
 [![MyGet](https://img.shields.io/myget/jason-c-daniels/v/Jcd.Validations?logo=nuget)](https://www.myget.org/feed/jason-c-daniels/package/nuget/Jcd.Validations)
-[![CodeFactor](https://www.codefactor.io/repository/github/jason-c-daniels/jcd.validations/badge/main)](https://www.codefactor.io/repository/github/jason-c-daniels/jcd.validations/overview/main)
+[![Nuget](https://img.shields.io/nuget/v/Jcd.Validations?logo=nuget)](https://www.nuget.org/packages/Jcd.Validations)
 
 [![API Docs](https://img.shields.io/badge/Read-The%20API%20Documentation-blue?style=for-the-badge)](https://github.com/jason-c-daniels/Jcd.Validations/blob/main/docs/Jcd_Validations.md)
